7f1fa7df73ef709c067f15a9a0b607e58f7fc890
星宿君 <huxinfeng1024@qq.com>
2023-07-05 17:42:33 +0800

    🐛 fix: 解决 GitHub Pages build 之后找不到文件 bug

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/gc-devtools/',
   outputPath: 'docs-dist',
+  publicPath: '/gc-devtools/',
   themeConfig: {
     name: 'gc-devtools',
   },
